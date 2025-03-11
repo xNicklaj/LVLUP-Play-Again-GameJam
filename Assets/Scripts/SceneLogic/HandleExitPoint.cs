f72595e226bf9fb5888902889521a6c57699b0f3
Simone Aronica <public.simonearonica@protonmail.ch>
2025-03-11 16:45:29 +0100

    Changed ExitPoint to trigger

--- Assets/Scripts/SceneLogic/HandleExitPoint.cs
+++ Assets/Scripts/SceneLogic/HandleExitPoint.cs
@@ -15,7 +15,7 @@ public class HandleExitPoint : NetworkBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.name.Contains("PlayerRecon")) return;
         if (GameManager_v2.Instance.GameStarted == false) return;
