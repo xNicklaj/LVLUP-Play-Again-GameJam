ca6036e35aac1cb12e41674d92168ceacc3f333c
Sdrucito <sdrucito94@gmail.com>
2025-03-14 17:58:39 +0100

    Minor fix to bossStuff after merge

--- Assets/Scripts/SceneLogic/BossAreaTrigger.cs
+++ Assets/Scripts/SceneLogic/BossAreaTrigger.cs
@@ -6,20 +6,9 @@ public class BossBattleTrigger : MonoBehaviour
     public int players=0;
     [SerializeField] private GameObject gateToClose;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Player entrato"+other.gameObject.name);
+        Debug.Log("Player entrato: "+other.gameObject.name);
         players++;
         if (players >= 3) {
             gateToClose.GetComponent<TilemapRenderer>().enabled = true;
